9445a65a735b54db366936be4ca211d63761188b
Brad Caron <bacaron@iu.edu>
2019-12-13 20:04:10 -0500

    Reason: output bvals bvecs

--- separate.sh
+++ separate.sh
@@ -12,11 +12,8 @@ do
 
 	# separate b0s from sos and sense data
 	if [ ${FOLDER} == "nodif" ]; then
-		dwiextract -bzero ${dwi} -fslgrad ${bvecs} ${bvals} ./${FOLDER}/dwi.nii.gz -nthreads $NCORE -force
+		dwiextract -bzero ${dwi} -fslgrad ${bvecs} ${bvals} ./${FOLDER}/dwi.nii.gz -export_grad_fsl ./${FOLDER}/dwi.bvecs ./${FOLDER}/dwi.bvals -nthreads $NCORE -force
 	else
-		dwiextract -no_bzero ${dwi} -fslgrad ${bvecs} ${bvals} ./${FOLDER}/dwi.nii.gz -nthreads $NCORE -force
+		dwiextract -no_bzero ${dwi} -fslgrad ${bvecs} ${bvals} ./${FOLDER}/dwi.nii.gz -export_grad_fsl ./${FOLDER}/dwi.bvecs ./${FOLDER}/dwi.bvals -nthreads $NCORE -force
 	fi
-
-	cp ${bvals} ./${FOLDER}/
-	cp ${bvecs} ./${FOLDER}/
 done
