69f1da66e3216f652dfce6550ea347c81093f446
Brad Caron <bacaron@iu.edu>
2021-02-13 15:30:13 -0500

    reason: fixes

--- separate.sh
+++ separate.sh
@@ -5,7 +5,7 @@ dwi=`jq -r '.dwi' config.json`
 bvals=`jq -r '.bvals' config.json`
 bvecs=`jq -r '.bvecs' config.json`
 shell=`jq -r '.shell' config.json`
-with_bzeros=`jq -r '.with_bzeros' config.json`
+with_bzeros=`jq -r '.with_bzero' config.json`
 folders="dwi"
 
 # make output dir
@@ -14,10 +14,19 @@ folders="dwi"
 # set bzero line. if true (default), will include bzeros. if false, will exclude
 if [[ ${with_bzeros} == 'true' ]]; then
 	bzero_line="-bzero"
+	shell_line="-shell 0,${shell}"
 else
 	bzero_line="-no_bzero"
+	shell_line="-shell ${shell}"
 fi
 
 # extract shell with bzeros
-dwiextract ${dwi} -fslgrad ${bvecs} ${bvals} ./${folders}/dwi.nii.gz -singleshell ${shell} ${bzero_line} -export_grad_fsl ./${folders}/dwi.bvecs ./${folders}/dwi.bvals -nthreads $NCORE -force
+dwiextract ${bzero_line} \
+	${shell_line} \
+	${dwi} \
+	-fslgrad ${bvecs} ${bvals} \
+	./${folders}/dwi.nii.gz \
+	-export_grad_fsl ./${folders}/dwi.bvecs ./${folders}/dwi.bvals \
+	-nthreads $NCORE \
+	-force
 
